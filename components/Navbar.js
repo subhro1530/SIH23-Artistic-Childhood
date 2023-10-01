40660e48998f7b29e1dbde29628180ae69755690
Pratham Agarwal <109911989+UniquePratham@users.noreply.github.com>
2023-10-01 17:31:48 +0530

    Ok

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,15 +3,23 @@
 import React from "react";
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css"; // Import the styles for the component
-
+import Image from 'next/image';
+import Spline from "@splinetool/react-spline";
+ 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
         {/* Replace 'path/to/your/3d-model-icon.png' with the actual path to your 3D model icon */}
-        <Link href="/">
-          
-        </Link>
+        {
+        /* <Image
+           src="/logo.png"
+           width={500}
+           height={500}
+           alt="Logo" 
+        /> */
+        <Spline scene="https://prod.spline.design/aCcqxGjkr4AmvfZt/scene.splinecode" />
+        }
       </div>
       <div className={styles.links}>
         <Link href="/gameplay">
