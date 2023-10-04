4bd7f03a4aebdfdfa290c10684b5e34e0d6986e6
Pratham Agarwal <109911989+UniquePratham@users.noreply.github.com>
2023-10-05 00:08:39 +0530

    Zoom adjusted

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,21 +5,28 @@ import Link from "next/link";
 import styles from "../styles/Navbar.module.css"; // Import the styles for the component
 import Image from 'next/image';
 import Spline from "@splinetool/react-spline";
- 
+import { Application } from '@splinetool/runtime'; 
+import { useState } from "react";
 const Navbar = () => {
+  const [myObj, setMyObj] = useState(null);
+  function onLoad(spline) {
+    const obj = spline.findObjectById('5bf7fb81-6117-422d-97d1-e247833be1ab');
+    // or
+    // const obj = spline.findObjectByName('my object');
+    spline.setZoom(0.1);
+    setMyObj(obj);
+  }
+  function moveObj() {
+    console.log(myObj); // Spline Object => { name: 'my object', id: '8E8C2DDD-18B6-4C54-861D-7ED2519DE20E', position: {}, ... }
+    // move the object in 3D space
+    myObj.position.x += 10;
+  }
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-        {/* Replace 'path/to/your/3d-model-icon.png' with the actual path to your 3D model icon */}
-        {
-        /* <Image
-           src="/logo.png"
-           width={500}
-           height={500}
-           alt="Logo" 
-        /> */
-        <Spline scene="https://prod.spline.design/aCcqxGjkr4AmvfZt/scene.splinecode" />
-        }
+        <Spline scene="https://prod.spline.design/aCcqxGjkr4AmvfZt/scene.splinecode" 
+            onLoad={onLoad} 
+         />
       </div>
       <div className={styles.links}>
         <Link href="/gameplay">
